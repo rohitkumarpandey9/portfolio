0068ea943fb7aa9c9be1983364c20614575141dc
Rohit Kumar Pandey <146954966+rohitkumarpandey9@users.noreply.github.com>
2026-06-01 14:00:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import ChatBot from './components/ChatBot'
 import ParticlesBg from './components/ParticlesBg'
 
 function App() {
@@ -71,6 +72,7 @@ function App() {
       <Projects />
       <Contact />
       <Footer />
+      <ChatBot />
     </>
   )
 }
